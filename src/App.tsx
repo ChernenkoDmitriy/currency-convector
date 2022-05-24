509b73c4325f1f713e9ad8a56a9df9cb1163666f
OrlovMaks <4014499@gmail.com>
2022-05-24 14:27:20 +0300

    add toast and app icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React, { FC } from 'react';
+import Toast from 'react-native-toast-message';
+import { toastConfig } from './libraries/toast/toastConfig';
 import { AppNavigator } from './navigation';
 import { UIProvider } from './UIProvider';
 
+
+
 export const App: FC = () => {
+
     return (
         <UIProvider>
             <AppNavigator />
+            <Toast config={toastConfig} />
         </UIProvider>
     );
 };
